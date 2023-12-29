ad3dd1ca00d6433aaaf414fc4aac7cd20a9a6a34
Hector Agudelo <haacny86@gmail.com>
2023-12-29 10:55:14 -0500

    created the files for the about pages and rename seo links

---
+++ src/pages/about_coaches.tsx
@@ -0,0 +1,42 @@
+// about-us.tsx
+
+// This file renders the "About Us" page.
+// It includes various components to display information about the company.
+
+import OurMission from '@/components/about_coaches/OurMission';
+import WhatWeDo from '@/components/about_coaches/WhatWeDo';
+import WhoweAre from '@/components/about_coaches/WhoweAre';
+import Backtotop from '@/components/common/Backtotop';
+import CommonHero from '@/components/common/CommonHero';
+import Footer from '@/components/common/Footer';
+import Header from '@/components/common/Header';
+import Seo from '@/components/common/Seo';
+import { SEO_CONFIG } from '@/utils/seoConfig';
+
+const AboutCoaches = () => {
+  // SEO
+  const hero = {
+    heading: 'Coaches',
+    title: 'Your Title Here',
+    subtitle: 'Your Subtitle Here',
+  };
+
+  return (
+    <>
+      <Seo pageSEO={SEO_CONFIG.aboutCoaches} />
+      <div className='overflow_hidden'>
+        <div className=' bg-about-hero bg-no-repeat bg-cover'>
+          <Header />
+          <CommonHero hero={hero} />
+        </div>
+        <WhatWeDo />
+        <OurMission />
+        <WhoweAre />
+        <Footer />
+        <Backtotop />
+      </div>
+    </>
+  );
+};
+
+export default AboutCoaches;
