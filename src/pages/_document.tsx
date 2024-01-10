e9b63e81a52974751bc7ba2c272ad170e6cd8409
Quentin Vidal <33239211+qisforq@users.noreply.github.com>
2024-01-10 18:12:19 -0500

    integrated google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-313XEDH6LT"></script>
+        <script 
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-313XEDH6LT');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
