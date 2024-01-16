ce45ccd9a0b15e2823d5013fe4335edcde33f148
Quentin Vidal <33239211+qisforq@users.noreply.github.com>
2024-01-16 17:44:40 -0500

    updated google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,29 +1,16 @@
 // This file is used to augment the HTML and body tags of the Next.js application.
 // It's useful for including global CSS or setting meta tags.
 
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-313XEDH6LT"></script>
-        <script 
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-313XEDH6LT');
-            `,
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
