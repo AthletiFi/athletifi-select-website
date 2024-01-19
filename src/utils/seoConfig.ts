8dd5fe013ad2cf4a6b11e23804b2842e10218e1f
Quentin Vidal <33239211+qisforq@users.noreply.github.com>
2024-01-18 20:53:20 -0500

    updated meta photos

--- src/utils/seoConfig.ts
+++ src/utils/seoConfig.ts
@@ -3,37 +3,40 @@
 export const SEO_CONFIG = {
   aboutCoaches: {
     title: "Coaches | Athletifi",
-    description: "Discover AthletiFi's mission to transform the soccer world with dynamic digital trading cards. Dive into our journey, partnerships, and vision for the future of soccer.",
-    websiteURL: "https://athleti.fi/about-us",
-    image: "/about_us_meta.png",
-  }, aboutProgram: {
+    description: "Meet the experienced and passionate coaching team behind AthletiFi Summer Select.",
+    websiteURL: "https://www.athletifiselect.com/about-coaches",
+    image: "/about_coaches_meta.jpg",
+  },
+  aboutProgram: {
     title: "Program | Athletifi",
-    description: "Discover AthletiFi's mission to transform the soccer world with dynamic digital trading cards. Dive into our journey, partnerships, and vision for the future of soccer.",
-    websiteURL: "https://athleti.fi/about-us",
-    image: "/about_us_meta.png",
-  }, aboutCards: {
+    description: "Discover the AthletiFi Summer Select Program - improve your soccer skills and become the best player you can be.",
+    websiteURL: "https://www.athletifiselect.com/about-program",
+    image: "/about_program_meta.jpg",
+  },
+  aboutCards: {
     title: "Player Cards | Athletifi",
-    description: "Discover AthletiFi's mission to transform the soccer world with dynamic digital trading cards. Dive into our journey, partnerships, and vision for the future of soccer.",
-    websiteURL: "https://athleti.fi/about-us",
-    image: "/about_us_meta.png",
+    description: "Learn about AthletiFi's' Player Cards. See how we track and showcase player achievements and progress.",
+    websiteURL: "https://www.athletifiselect.com/about-cards",
+    image: "/player_cards_meta.jpg",
   },
   home: {
-    title: "Athletifi | Summer Select 2024",
-    description: " Premier Summer Training and Year-Round Development Programs for U9-U15 Players. Welcome to the future of sports.",
-    websiteURL: "https://athleti.fi",
-    image: "/lending_meta_img.webp",
+    title: "Athletifi Select",
+    description: "Join AthletiFi Select for an unrivaled soccer development experience. High-quality coaching, specialized training, and unique player growth opportunities await.",
+    websiteURL: "https://www.athletifiselect.com",
+    image: "/about_coaches_meta.png",
   },
   signUp: {
-    title: "Start Your Journey | Join AthletiFi",
-    description: "Signup for exclusive updates! Become part of the sport's revolution.",
-    websiteURL: "https://athleti.fi/sign-up",
-    image: "/sign_up_meta",
+    title: "Sign up for AthletiFi Select",
+    description: "Enroll now in the AthletiFi Summer Select Program. Secure a spot for your young athlete in our elite soccer development program.",
+    websiteURL: "https://www.athletifiselect.com/sign-up",
+    image: "/sign_up_meta.jpg",
   },
-  news: {
-    title: "AthletiFi News: Latest Updates & Announcements",
-    description: "(dynamically generated)", 
-    websiteURL: "https://athletif.fi",
-    image: "(dynamically generated", 
-  },
+  // news: {
+  //   title: "AthletiFi News: Latest Updates & Announcements",
+  //   description: "(dynamically generated)", 
+  //   websiteURL: "https://athletif.fi",
+  //   image: "(dynamically generated", 
+  // },
 };
+
 
